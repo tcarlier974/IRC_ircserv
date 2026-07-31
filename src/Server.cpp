65a9db4aa24ad15fe4679390374702df754c9be7
Tristan Carlier <carliertiti97459@gmail.com>
2026-07-31 13:03:28 +0200

    Broadcast MODE +o/-o changes to channel
    MODE handling for channel operators now sends standard MODE messages to all channel members instead of replying with 324/353/366 numerics to the issuing client. It also fixes de-opping the wrong user by applying `-o` to the mode argument target (`argIt`) rather than the requesting client.

--- src/Server.cpp
+++ src/Server.cpp
@@ -6,7 +6,7 @@
 /*   By: tcarlier <tcarlier@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/05 15:18:38 by tcarlier          #+#    #+#             */
-/*   Updated: 2026/07/31 12:03:09 by tcarlier         ###   ########.fr       */
+/*   Updated: 2026/07/31 12:56:54 by tcarlier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -468,7 +468,7 @@ void Server::ParseMessage(std::string message, Client *client)
 				count++;
 				iss >> mode;
 			}
-			(void)count; // Pour éviter l'avertissement de variable non utilisée
+			(void)count;
 			char sign = '+';
 			for (std::vector<std::string>::iterator it = modes.begin(); it != modes.end(); ++it)
 			{
@@ -487,17 +487,14 @@ void Server::ParseMessage(std::string message, Client *client)
 						{
 							targetChannel->SetOP(argIt->c_str());
 							printf("Client %s is now an operator in channel %s\n", argIt->c_str(), channel.c_str());
-							client->AppendOutBuffer(":ircserv 324 " + client->GetNickname() + " " + channel + " +o\r\n");
-							std::string rpl_namreply = ":ircserv 353 " + client->GetNickname() + " = " + targetChannel->GetName() + " :" + targetChannel->GetMemberList() + "\r\n";
-							client->AppendOutBuffer(rpl_namreply);
-							std::string rpl_endofnames = ":ircserv 366 " + client->GetNickname() + " " + targetChannel->GetName() + " :End of /NAMES list.\r\n";
-							client->AppendOutBuffer(rpl_endofnames);
+							targetChannel->BroadcastMessage(":" + client->GetNickname() + "!" + client->GetUsername() + "@" + client->GetIPadd() + " " + "MODE " + channel.c_str() + " " + "+o " + argIt->c_str() + "\r\n");
+							argIt++;
 						}
 						else
 						{
-							targetChannel->DeOP(client);
+							targetChannel->DeOP(argIt->c_str());
+							targetChannel->BroadcastMessage(":" + client->GetNickname() + "!" + client->GetUsername() + "@" + client->GetIPadd() + " " + "MODE " + channel.c_str() + " " + "-o " + argIt->c_str() + "\r\n");
 							argIt++;
-							client->AppendOutBuffer(":ircserv 324 " + client->GetNickname() + " " + channel + " -o\r\n");
 						}
 						break;
 					default:
